6d71846f9b74e17052dff0b0873fa0d5039fae95
Wayne Lewis <waynelewis@esss.se>
2018-12-04 19:48:43 +0100

    ENH: Add detector-interface functionality - changes by Will Smith

--- st.hzb-v20-evr-02.cmd
+++ st.hzb-v20-evr-02.cmd
@@ -2,93 +2,83 @@ epicsEnvSet("SYS", "HZB-V20:TS")
 epicsEnvSet("PCI_SLOT", "1:0.0")
 epicsEnvSet("DEVICE", "EVR-02")
 epicsEnvSet("EVR", "$(DEVICE)")
-epicsEnvSet("CHIC_SYS", "HZB-V20:")
-epicsEnvSet("CHOP_DRV", "Chop-Drv-02tmp")
-epicsEnvSet("CHIC_DEV", "TS-$(DEVICE)")
 epicsEnvSet("MRF_HW_DB", "evr-pcie-300dc-ess.db")
-#epicsEnvSet("E3_MODULES", "/epics/iocs/e3")
+epicsEnvSet("E3_MODULES", "/epics/iocs/e3")
 epicsEnvSet("EPICS_CMDS", "/epics/iocs/cmds")
 
 ######## Temporary until chopper group ###########
 ######## changes PV names              ###########
-epicsEnvSet("NCG_SYS", "HZB-V20:")
+#epicsEnvSet("NCG_SYS", "HZB-V20:")
 # Change to 02a: to avoid conflict with EVR2 names
-epicsEnvSet("NCG_DRV", "Chop-Drv-02tmp:")
+#epicsEnvSet("NCG_DRV", "Chop-Drv-02tmp:")
 ##################################################
 
 < "$(EPICS_CMDS)/mrfioc2-common-cmd/st.evr.cmd"
+
+require dmsc_detector_interface,master
+require stream,2.7.14p
+
+
+############# -------- Detector Readout Interface ----------------- ##################
+epicsEnvSet("DETINT_CMD_TOP","/epics/iocs/cmds/hzb-v20-evr-02-cmd") 
+#epicsEnvSet("DETINT_DB_TOP", "$(E3_MODULES)/e3-detectorinterface/m-epics-detectorinterface-dev/db")
+epicsEnvSet("STREAM_PROTOCOL_PATH","/epics/base-7.0.1.1/require/3.0.4/siteApps/dmsc_detector_interface/master/db")
+
+epicsEnvSet("DET_CLK_RST_EVT", "15")
+epicsEnvSet("DET_RST_EVT", "15")
+epicsEnvSet("SYNC_EVNT_LETTER", "EvtF")
+epicsEnvSet("SYNC_TRIG_EVT", "16")
+
+epicsEnvSet("COM1_USB_DEV_NUM", "0")
+epicsEnvSet("COM2_USB_DEV_NUM", "2")
+
+
+
+# Load the detector interface module
+iocshLoad("$(DETINT_CMD_TOP)/detint.cmd", "DEV1=RO1, DEV2=RO2, COM1=COM1, COM2=COM2,COM1_USB_DEV_NUM=$(COM1_USB_DEV_NUM),COM2_USB_DEV_NUM=$(COM2_USB_DEV_NUM), SYS=$(SYS), SYNC_EVNT=$(DET_RST_EVT), SYNC_EVNT_LETTER=$(SYNC_EVNT_LETTER), N_SEC_TICKS=1000000000 ")
+
+
+
+iocInit()
+
+# Global default values
+# Set the frequency that the EVR expects from the EVG for the event clock
+dbpf $(SYS)-$(DEVICE):Time-Clock-SP 88.0525
+
 
 # Set delay compensation target. This is required even when delay compensation
 # is disabled to avoid occasionally corrupting timestamps.
 #dbpf $(SYS)-$(DEVICE):DC-Tgt-SP 70
 dbpf $(SYS)-$(DEVICE):DC-Tgt-SP 100
 
-######### INPUTS #########
+# Connect prescaler reset to event DET_CLK_RST_EVT
+dbpf $(SYS)-$(DEVICE):Evt-ResetPS-SP DET_CLK_RST_EVT
 
-# Set up of UnivIO 0 as Input. Generate Code 10 locally on rising edge.
-dbpf $(SYS)-$(DEVICE):In0-Lvl-Sel "Active High"
-dbpf $(SYS)-$(DEVICE):In0-Edge-Sel "Active Rising"
-dbpf $(SYS)-$(DEVICE):OutFPUV00-Src-SP 61
-dbpf $(SYS)-$(DEVICE):In0-Trig-Ext-Sel "Edge"
-dbpf $(SYS)-$(DEVICE):In0-Code-Ext-SP 10
-dbpf $(SYS)-$(DEVICE):EvtA-SP.OUT "@OBJ=$(EVR),Code=10"
-dbpf $(SYS)-$(DEVICE):EvtA-SP.VAL 10
+# Connect FP08 to PS0
+dbpf $(SYS)-$(DEVICE):OutFPUV08-Ena-SP 1
+dbpf $(SYS)-$(DEVICE):OutFPUV08-Src-SP 40 
+dbpf $(SYS)-$(DEVICE):PS0-Div-SP 2
 
-# Set up of UnivIO 1 as Input. Generate Code 11 locally on rising edge.
-dbpf $(SYS)-$(DEVICE):In1-Lvl-Sel "Active High"
-dbpf $(SYS)-$(DEVICE):In1-Edge-Sel "Active Rising"
-dbpf $(SYS)-$(DEVICE):OutFPUV01-Src-SP 61
-dbpf $(SYS)-$(DEVICE):In1-Trig-Ext-Sel "Edge"
-dbpf $(SYS)-$(DEVICE):In1-Code-Ext-SP 11
-dbpf $(SYS)-$(DEVICE):EvtB-SP.OUT "@OBJ=$(EVR),Code=11"
-dbpf $(SYS)-$(DEVICE):EvtB-SP.VAL 11
+# Map pulser 9 to event code SYNC_TRIG_EVT
+dbpf $(SYS)-$(DEVICE):DlyGen9-Evt-Trig0-SP 16
+dbpf $(SYS)-$(DEVICE):DlyGen9-Width-SP 10
 
-######### OUTPUTS #########
-dbpf $(SYS)-$(DEVICE):DlyGen1-Evt-Trig0-SP 14
-dbpf $(SYS)-$(DEVICE):DlyGen1-Width-SP 2860 #1ms
-dbpf $(SYS)-$(DEVICE):DlyGen1-Delay-SP 0 #0ms
-dbpf $(SYS)-$(DEVICE):OutFPUV03-Src-SP 1 #Connect output2 to DlyGen-1
+# Connect FP09 to Pulser 9
+dbpf $(SYS)-$(DEVICE):OutFPUV09-Ena-SP 1
+dbpf $(SYS)-$(DEVICE):OutFPUV09-Src-SP 9 
 
-#Set up delay generator 2 to trigger on event 16
-dbpf $(SYS)-$(DEVICE):DlyGen2-Width-SP 1000 #1ms
-dbpf $(SYS)-$(DEVICE):DlyGen2-Delay-SP 0 #0ms
-dbpf $(SYS)-$(DEVICE):DlyGen2-Evt-Trig0-SP 16
+# Connect FP10 to PS0
+dbpf $(SYS)-$(DEVICE):OutFPUV10-Ena-SP 1
+dbpf $(SYS)-$(DEVICE):OutFPUV10-Src-SP 40 
 
-#Set up delay generator 0 to trigger on event 17 and set universal I/O 2
-dbpf $(SYS)-$(DEVICE):DlyGen0-Width-SP 1000 #1ms
-dbpf $(SYS)-$(DEVICE):DlyGen0-Delay-SP 0 #0ms
-dbpf $(SYS)-$(DEVICE):DlyGen0-Evt-Trig0-SP 17
-dbpf $(SYS)-$(DEVICE):OutFPUV02-Src-SP 0 #Connect to DlyGen-0
+# Connect FP11 to Pulser 9
+dbpf $(SYS)-$(DEVICE):OutFPUV11-Ena-SP 1
+dbpf $(SYS)-$(DEVICE):OutFPUV11-Src-SP 9 
 
-######## Sequencer #########
-# Select trigger source for soft seq 0, trigger source 0, 2 means pulser 2
-dbpf $(SYS)-$(DEVICE):SoftSeq0-TrigSrc-0-Sel 2
+# Connect FP2 to Pulser 9
+dbpf $(SYS)-$(DEVICE):OutFPUV02-Ena-SP 1
+dbpf $(SYS)-$(DEVICE):OutFPUV02-Src-SP 9 
 
-# Load sequencer setup
-dbpf $(SYS)-$(DEVICE):SoftSeq0-Load-Cmd 1
-
-# Enable sequencer
-dbpf $(SYS)-$(DEVICE):SoftSeq0-Enable-Cmd 1
-
-dbpf $(CHIC_SYS)$(CHOP_DRV)01:Freq-SP 28
-dbpf $(CHIC_SYS)$(CHOP_DRV)02:Freq-SP 28
-dbpf $(CHIC_SYS)$(CHOP_DRV)03:Tube-Pos-Delay 10
-dbpf $(CHIC_SYS)$(CHOP_DRV)04:Tube-Pos-Delay 20
-# Check that this command is required.
-dbpf $(SYS)-$(DEVICE):RF-Freq 88052500
-
-# Hints for setting input PVs from client
-#caput -a $(SYS)-$(DEVICE):SoftSeq0-EvtCode-SP 2 17 18
-#caput -a $(SYS)-$(DEVICE):SoftSeq0-Timestamp-SP 2 0 12578845
-#caput -n $(SYS)-$(DEVICE):SoftSeq0-Commit-Cmd 1
-
-######### TIME STAMP #########
-
-#Forward links to esschicTimestampBuffer.template
-#dbpf $(SYS)-$(DEVICE):EvtACnt-I.FLNK $(CHIC_SYS)$(CHOP_DRV):TDC
-#dbpf $(SYS)-$(DEVICE):EvtECnt-I.FLNK $(CHIC_SYS)$(CHOP_DRV):Ref
-
-#dbpf $(SYS)-$(DEVICE):EvtBCnt-I.FLNK $(CHIC_SYS)$(CHOP_DRV):TDC
-#dbpf $(CHIC_SYS)$(CHOP_DRV)01:BPFO.LNK3 $(CHIC_SYS)$(CHOP_DRV):Ref
-
+dbpf $(SYS)-$(DEVICE):OutFPUV03-Ena-SP 1
+dbpf $(SYS)-$(DEVICE):OutFPUV03-Src-SP 40 
 
